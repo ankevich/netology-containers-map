859ddcba1ed4f7d8ac90fefb7c417bea84f4837f
Annie Bauer <overheads165@gmail.com>
2022-07-21 15:34:23 +0300

    map

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+class ErrorRepository {
+  static errors = new Map([
+    [404, "Not Found"],
+    [200, "OK"],
+  ]);
+
+  static translate = (errorCode) => {
+    if (this.errors.has(errorCode)) {
+      return this.errors.get(errorCode);
+    } else {
+      return "Unknown error";
+    }
+  };
+}
+
+console.log(ErrorRepository.errors);
+console.log(ErrorRepository.translate(404));
+console.log(ErrorRepository.translate(200));
+console.log(ErrorRepository.translate(900));
